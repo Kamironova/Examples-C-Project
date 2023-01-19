635e112b22f911f3f7cbeef88d47f22d5144d3f4
Karina <kamironova09@gmail.com>
2023-01-19 14:39:29 +0300

    Example 005 condition if-alse

---
+++ Examples/Examples005_CoditionIf/Program.cs
@@ -0,0 +1,21 @@
+﻿//Console.Write("Введите имя");
+//String username = Console.ReadLine();
+//if username = "Маша" then;
+//Console.Write("Ура, Это же Маша!");
+//else Console.Write("Привет, ");
+//Console.Write(username):
+
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.Write("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
+    
