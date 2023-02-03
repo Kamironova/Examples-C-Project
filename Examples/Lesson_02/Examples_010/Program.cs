5c0ab5cc869fc054877a999148742afc3b9e67c7
Karina <kamironova09@gmail.com>
2023-02-03 16:11:12 +0300

    example 010

---
+++ Examples/Lesson_02/Examples_010/Program.cs
@@ -0,0 +1,24 @@
+﻿// Имеется одномерный массив array из n эллемннтов, требуется найти эллемент
+// массива, равный find
+
+Console.Clear();
+
+int[] array = { 11, 22, 93, 49, 93, 71, 77, 8 };
+
+int n = array.Length;
+int find = 93;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;  // этот оператор прерывает поиск в случае если найдет значение( иначе он может найти второе такое же)
+
+    }
+    index++;
+
+
+}
