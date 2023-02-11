f50f669b910e72574e1a2b0f9f16f3084d5dfaca
Карина Миронова <kamironova@192.168.1.9>
2023-02-11 17:25:10 +0300

    Metods & Cycles

---
+++ Examples/Lesson_03/Examples_012_1_Methods/Program.cs
@@ -0,0 +1,31 @@
+﻿// ==== Работа с текстом
+// Дан текст, в тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить маленькими "с".
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежали бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+
+string Replase(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replase(text, ' ', '|');
+System.Console.WriteLine(newText);
+System.Console.WriteLine();
+
+newText = Replase(newText, 'к', 'К');
+System.Console.WriteLine(newText);
