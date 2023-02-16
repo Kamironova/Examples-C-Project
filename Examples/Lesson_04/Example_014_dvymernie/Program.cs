7eeaae0c8b7092f198e997476913d11d544f96bc
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-02-16 12:24:56 +0300

    lesson 4

---
+++ Examples/Lesson_04/Example_014_dvymernie/Program.cs
@@ -0,0 +1,36 @@
+﻿// Пример создания двуиерного массива
+
+Console.Clear();
+
+//int[,] matrix = new int[3,4];  // где 3 это количество строк, а 4 - столбцов
+
+void PrintArray(int[,] matr)  // метод для создания двумерного массива
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // matr(matrix)GetLength(0), 0 означает длину строки
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)  // 1 - длина столбца
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+    
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+} 
+
+int[,] matrix = new int[3,4];
+
+PrintArray(matrix);
+FillArray(matrix);
+System.Console.WriteLine();
+PrintArray(matrix);
